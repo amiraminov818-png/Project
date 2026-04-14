d9e0cc085277acd61df957d12cddd9dc1c61c257
Ваше Имя <amiraminov818@gmail.com>
2026-04-14 17:12:35 +0500

    migration fixes

--- database/migrations/0001_01_01_200000_create_users_table.php
+++ database/migrations/0001_01_01_200000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->foreignIdFor(Role::class, 'role_id')
-                ->constrained('roles');
+                ->default(1)->constrained('roles');
             $table->rememberToken();
             $table->timestamps();
         });
